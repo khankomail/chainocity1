f889b6e28bb0d12ea5711fb68ca7b52c0ff0b03d
khankomail <cobraking1.ck@gmail.com>
2023-12-08 02:43:41 +0500

    before hosting

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import ChainUser,Product,Payment
+from .models import ChainUser,Product,Payment,Receipt
 
 # Register your models here.
 admin.site.register(ChainUser)
 admin.site.register(Product)
 admin.site.register(Payment)
+admin.site.register(Receipt)
 
